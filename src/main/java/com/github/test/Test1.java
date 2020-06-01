af0d0d2f63f16fba234a19629917acbd110f0c01
BG387801 <bzhang01@best-inc.com>
2020-06-01 17:07:05 +0800

    测试

---
+++ src/main/java/com/github/test/Test1.java
@@ -0,0 +1,15 @@
+package com.github.test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test1 test = new Test1();
+		test.helloWord("aa");
+	}
+	
+	public void helloWord(String name) {
+		System.out.println(name + ": Hello!");
+	}
+
+}
